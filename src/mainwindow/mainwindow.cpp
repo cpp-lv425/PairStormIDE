9b56219d706a08527326ec662ca03871e25138bf
Volodymyr Sakharevych <vsakharevych@gmail.com>
2019-07-23 12:41:57 +0300

    added MainMenu of the MainWindow

--- src/mainwindow/mainwindow.cpp
+++ src/mainwindow/mainwindow.cpp
@@ -1,14 +1,209 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QStyleFactory>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // when first started main window is maximized
+    setWindowState(Qt::WindowMaximized);
+
+    // set Fusion style globally - TEMP SOLUTION
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+
+    setMainMenu();
+}
+
+
+void MainWindow::setMainMenu()
+{
+    // file menu
+    QMenu *fileMenu = new QMenu("&File");
+    fileMenu->addAction("&New file", this, &MainWindow::newFileTriggered, Qt::CTRL + Qt::Key_N);
+    fileMenu->addAction("&Open file...", this, &MainWindow::openFileTriggered, Qt::CTRL + Qt::Key_O);
+    fileMenu->addAction("Open &folder...", this, &MainWindow::openFolderTriggered);
+    fileMenu->addAction("Start &Page...", this, &MainWindow::openStartPage);
+    fileMenu->addSeparator();
+    fileMenu->addAction("&Save...", this, &MainWindow::saveFileTriggered, Qt::CTRL + Qt::Key_S);
+    fileMenu->addAction("Save &As...", this, &MainWindow::saveFileAsTriggered, Qt::CTRL + Qt::Key_S);
+    fileMenu->addAction("Save A&ll...", this, &MainWindow::saveAllFilesTriggered, Qt::CTRL + Qt::SHIFT + Qt::Key_S);
+    fileMenu->addSeparator();
+    fileMenu->addAction("&Close file", this, &MainWindow::closeFileTriggered, Qt::CTRL + Qt::Key_W);
+    fileMenu->addAction("&Exit", this, &MainWindow::close, Qt::ALT + Qt::Key_F4);
+
+    // edit menu
+    QMenu *editMenu = new QMenu("&Edit");
+    editMenu->addAction("&Undo", this, &MainWindow::undoTriggered, Qt::CTRL + Qt::Key_Z);
+    editMenu->addAction("&Redo", this, &MainWindow::redoTriggered, Qt::CTRL + Qt::Key_Y);
+    editMenu->addSeparator();
+    editMenu->addAction("Cu&t", this, &MainWindow::cutTriggered, Qt::CTRL + Qt::Key_X);
+    editMenu->addAction("&Copy", this, &MainWindow::copyTriggered, Qt::CTRL + Qt::Key_C);
+    editMenu->addAction("&Paste", this, &MainWindow::pasteTriggered, Qt::CTRL + Qt::Key_V);
+    editMenu->addAction("Select &All", this, &MainWindow::selectAllTriggered, Qt::CTRL + Qt::Key_A);
+    editMenu->addSeparator();
+    editMenu->addAction("&Find/Replace...", this, &MainWindow::findTriggered, Qt::CTRL + Qt::Key_F);
+
+    // view menu
+    QMenu *viewMenu = new QMenu("&View");
+    viewMenu->addAction("&Full screen", this, &MainWindow::fullScreenTriggered, Qt::CTRL + Qt::SHIFT + Qt::Key_F11);
+    QMenu *scaleSubMenu = new QMenu("&Scale");
+    viewMenu->addMenu(scaleSubMenu);
+
+    // tools menu
+    QMenu *toolsMenu = new QMenu("&Tools");
+    toolsMenu->addAction("&Refactor...", this, &MainWindow::refactorTriggered);
+    toolsMenu->addSeparator();
+    toolsMenu->addAction("&Connect...", this, &MainWindow::connectTriggered);
+    toolsMenu->addSeparator();
+    QMenu *buildSubMenu = new QMenu("&Build");
+    toolsMenu->addMenu(buildSubMenu);
+    QMenu *debugSubMenu = new QMenu("&Debug");
+    toolsMenu->addMenu(debugSubMenu);
+    QMenu *gitSubMenu = new QMenu("&Git");
+    toolsMenu->addMenu(gitSubMenu);
+    toolsMenu->addSeparator();
+    toolsMenu->addAction("&Settings...", this, &MainWindow::settingsTriggered);
+
+    // help menu
+    QMenu *helpMenu = new QMenu("&Help");
+    helpMenu->addAction("&About...", this, &MainWindow::aboutTriggered);
+    helpMenu->addSeparator();
+    helpMenu->addAction("&Reference Assistant...", this, &MainWindow::referenceTriggered, Qt::CTRL + Qt::Key_F1);
+    helpMenu->addAction("User &Guide...", this, &MainWindow::userGuideTriggered, Qt::Key_F1);
+    helpMenu->addSeparator();
+    helpMenu->addAction("Check for &Updates...", this, &MainWindow::checkUpdatesTriggered);
+
+    // adding all menus to MainMenuBar
+    menuBar()->addMenu(fileMenu);
+    menuBar()->addMenu(editMenu);
+    menuBar()->addMenu(viewMenu);
+    menuBar()->addMenu(toolsMenu);
+    menuBar()->addMenu(helpMenu);
+}
+
+void MainWindow::newFileTriggered()
+{
+    qDebug() << "newFileTriggered";
+}
+
+void MainWindow::openFileTriggered()
+{
+    qDebug() << "open file";
+}
+
+void MainWindow::openFolderTriggered()
+{
+    qDebug() << "open folder";
+}
+
+void MainWindow::openStartPage()
+{
+    qDebug() << "open start page";
+}
+
+void MainWindow::saveFileTriggered()
+{
+    qDebug() << "save file";
+}
+
+void MainWindow::saveFileAsTriggered()
+{
+    qDebug() << "save as";
+}
+
+void MainWindow::saveAllFilesTriggered()
+{
+    qDebug() << "save all";
+}
+
+void MainWindow::closeFileTriggered()
+{
+    qDebug() << "close file";
+}
+
+void MainWindow::undoTriggered()
+{
+    qDebug() << "undo";
+}
+
+void MainWindow::redoTriggered()
+{
+    qDebug() << "redo";
+}
+
+void MainWindow::cutTriggered()
+{
+    qDebug() << "cut";
+}
+
+void MainWindow::copyTriggered()
+{
+    qDebug() << "copy";
+}
+
+void MainWindow::pasteTriggered()
+{
+    qDebug() << "paste";
+}
+
+void MainWindow::selectAllTriggered()
+{
+    qDebug() << "select all";
+}
+
+void MainWindow::findTriggered()
+{
+    qDebug() << "find";
+}
+
+void MainWindow::fullScreenTriggered()
+{
+    qDebug() << "full screen";
+}
+
+void MainWindow::refactorTriggered()
+{
+    qDebug() << "refactor";
+}
+
+void MainWindow::connectTriggered()
+{
+    qDebug() << "connect";
+}
+
+void MainWindow::settingsTriggered()
+{
+    qDebug() << "settings";
+}
+
+void MainWindow::aboutTriggered()
+{
+    qDebug() << "about";
+}
+
+void MainWindow::referenceTriggered()
+{
+    qDebug() << "reference";
+}
+
+void MainWindow::userGuideTriggered()
+{
+    qDebug() << "user guide";
+}
+
+void MainWindow::checkUpdatesTriggered()
+{
+    qDebug() << "check updates";
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
--- src/mainwindow/mainwindow.h
+++ src/mainwindow/mainwindow.h
@@ -7,6 +7,12 @@ namespace Ui {
 class MainWindow;
 }
 
+QT_BEGIN_NAMESPACE
+class QDockWidget;
+class QFileSystemModel;
+class QMdiArea;
+QT_END_NAMESPACE
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -17,6 +23,46 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QDockWidget *mpExplorerDock;
+    QDockWidget *mpChatWindowDock;
+    QDockWidget *mpBottomPanelDock;
+    QMdiArea *mpDocsArea;
+
+    void setMainMenu();
+
+private slots:
+    // file menu actions
+    void newFileTriggered();
+    void openFileTriggered();
+    void openFolderTriggered();
+    void openStartPage();
+    void saveFileTriggered();
+    void saveFileAsTriggered();
+    void saveAllFilesTriggered();
+    void closeFileTriggered();
+
+    // edit menu actions
+    void undoTriggered();
+    void redoTriggered();
+    void cutTriggered();
+    void copyTriggered();
+    void pasteTriggered();
+    void selectAllTriggered();
+    void findTriggered();
+
+    // view menu
+    void fullScreenTriggered();
+
+    // tools menu
+    void refactorTriggered();
+    void connectTriggered();
+    void settingsTriggered();
+
+    // help menu
+    void aboutTriggered();
+    void referenceTriggered();
+    void userGuideTriggered();
+    void checkUpdatesTriggered();
 };
 
 #endif // MAINWINDOW_H
