b488bb4c37bcbb9bd063454fb3a86256a575b229
Valentyn Faychuk <Valentyn.Faichuk.TK.2015@edu.lpnu.ua>
2019-07-24 18:52:53 +0300

    added apiparser baseground

--- src/networking/apiparser.cpp
+++ src/networking/apiparser.cpp
@@ -1,6 +1,31 @@
 #include "apiparser.h"
 
-ApiParser::ApiParser(QObject *parent) : QObject(parent)
+BaseApiParser::BaseApiParser(QObject *qObject) : QObject(qObject)
 {
+    return;
+}
 
+ApiParser::ApiParser() : BaseApiParser()
+{
+    m_udpService = UdpService::getInstance();
+    m_tcpService = TcpService::getInstance();
+    connect(
+        m_udpService.get(), &UdpService::processReceivedDatagram,
+        this,               &ApiParser::processUdpDatagramOnReceive,
+        Qt::UniqueConnection
+    );
+    // TODO connect to tcp service also
+}
+
+void ApiParser::testSendDatagram()
+{
+    m_udpService->broadcastBeaconDatagram();
+}
+
+void ApiParser::processUdpDatagramOnReceive()
+{
+    qDebug() << "udp datagram in api parser:";
+    qDebug() << "____________________________________________________________";
+    qDebug() << m_udpService->getReceivedDatagram();
+    qDebug() << "____________________________________________________________";
 }
--- src/networking/apiparser.h
+++ src/networking/apiparser.h
@@ -1,17 +1,41 @@
 #ifndef APIPARSER_H
 #define APIPARSER_H
 
-#include <QObject>
 
-class ApiParser : public QObject
+#include <QObject>
+#include "udpservice.h"
+#include "tcpservice.h"
+
+class BaseApiParser : public QObject
 {
     Q_OBJECT
+
+protected:
+    std::shared_ptr<UdpService> m_udpService;
+    std::shared_ptr<TcpService> m_tcpService;
+
 public:
-    explicit ApiParser(QObject *parent = nullptr);
+    explicit BaseApiParser(QObject *qObject = nullptr);
+
+    virtual void testSendDatagram() = 0;
+
+public slots:
+    virtual void processUdpDatagramOnReceive() = 0;
+};
+
+class ApiParser : public BaseApiParser
+{
+    Q_OBJECT
+
+public:
+    explicit ApiParser();
+
+    virtual void testSendDatagram() override;
 
 signals:
 
 public slots:
+    virtual void processUdpDatagramOnReceive() override;
 };
 
 #endif // APIPARSER_H
