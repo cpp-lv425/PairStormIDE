3fb504ce73c38e6f69d53cbbeb55f987f49db8ac
opastushkov <oleks.pastushkov@gmail.com>
2019-07-26 12:58:19 +0300

    Add types of lexem

---
+++ src/editor/type.h
@@ -0,0 +1,15 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+enum Type {
+    IDENTIFIER,
+    KW,
+    NUMBER,
+    FLOAT_NUMBER,
+    OPERATOR,
+    LITERAL,
+    COMMENT,
+    UNDEFINED
+};
+
+#endif // TYPE_H
