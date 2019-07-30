44b7ae1fef612025d79d01d15cce37dd127d23e6
DANIV18 <igordaniv242@gmail.com>
2019-07-30 17:50:35 +0300

    Fixed search function

--- src/documentation/documentationsearch.cpp
+++ src/documentation/documentationsearch.cpp
@@ -1,9 +1,33 @@
 #include "documentationsearch.h"
 
+#include <QDebug>
 #include <QDirIterator>
 QVector<QFileInfo> DocumentationSearch::search(const QDir &directory, const QString &keyword)
 {
     QVector<QFileInfo> rResult;
-    QDirIterator it(directory);
+    QDirIterator it(directory, QDirIterator::Subdirectories);
+    while (it.hasNext())
+    {
+        if(it.fileInfo().suffix() == "html" && it.fileInfo().baseName()== keyword)
+        {
+             rResult.push_back(it.fileInfo());
+        }
+        it.next();
+    }
+    return rResult;
+}
 
+QVector<QFileInfo> DocumentationSearch::search(const QDir &directory, const QString &include, const QString &keyword)
+{
+    QVector<QFileInfo> rResult;
+    QDirIterator it(directory, QDirIterator::Subdirectories);
+    while (it.hasNext())
+    {
+        if(it.fileInfo().suffix() == "html" && it.fileInfo().baseName()== keyword && it.fileInfo().dir().dirName() == include)
+        {
+             rResult.push_back(it.fileInfo());
+        }
+        it.next();
+    }
+    return rResult;
 }
