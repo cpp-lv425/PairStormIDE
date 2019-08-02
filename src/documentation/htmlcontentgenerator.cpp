fcb8f18a661567c3882c641dc8554af8332f10c5
DANIV18 <igordaniv242@gmail.com>
2019-08-02 12:35:49 +0300

    Fixed filename view

--- src/documentation/htmlcontentgenerator.cpp
+++ src/documentation/htmlcontentgenerator.cpp
@@ -3,18 +3,19 @@
 #include <mustache.hpp>
 
 #include <QUrl>
+#include <QDir>
 
 static std::string mustasheTemplate{R"(<!DOCTYPE html>
                                     <html>
                                         <head>
-                                            <title>SearchResults</title>
+                                            <title>{{search}}</title>
                                             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                                             </head>
                                             <body>
                                                 <p>&nbsp;</p>
-                                                <h1>Search Results</h1>
+                                                <h1>{{search}}</h1>
                                                 <p>&nbsp;</p>
-                                                <p>&nbsp;</p>
+
                                                 <table style="width:100%">
                                                     <tbody>
                                                         <tr>
@@ -71,18 +72,14 @@ static std::string mustasheTemplate{R"(<!DOCTYPE html>
                                                                  )"};
 
 std::string HTMLContentGenerator::generate(const QVector<QFileInfo> &files)
-{    //return mustasheTemplate;
-    const bool showIconsColumn = true, showSizeColumn = true, showDetailsConst = true;
+{
+
+    const bool showIconsColumn = true;
     kainjow::mustache::mustache tmpl{mustasheTemplate};
 
     kainjow::mustache::data mustashe{};
     mustashe.set("showIcons", showIconsColumn ? kainjow::mustache::data::type::bool_true
                                               : kainjow::mustache::data::type::bool_false);
-    mustashe.set("showSize", showSizeColumn ? kainjow::mustache::data::type::bool_true
-                                            : kainjow::mustache::data::type::bool_false);
-    mustashe.set("showDetails", showDetailsConst ? kainjow::mustache::data::type::bool_true
-                                                 : kainjow::mustache::data::type::bool_false);
-
 
     kainjow::mustache::data iconsListMustashe{kainjow::mustache::data::type::list};
     kainjow::mustache::data fileNamesListMustashe{kainjow::mustache::data::type::list};
@@ -102,12 +99,12 @@ std::string HTMLContentGenerator::generate(const QVector<QFileInfo> &files)
 
             if (!files.at(fileIndex).isDir())
             {
-                    dirEntryMustashe.set("fileIcon", R"(<i class="fa fa-level-up"></i>)");
+                    dirEntryMustashe.set("fileIcon", R"(<i class="fas fa-code"></i>)");
 
             }
             else
             {
-                dirEntryMustashe.set("fileIcon", R"(<i class="fa fa-folder"></i>)");
+                dirEntryMustashe.set("fileIcon", R"(<i class="fas fa-code"></i>)");
             }
         }
         else
@@ -116,9 +113,11 @@ std::string HTMLContentGenerator::generate(const QVector<QFileInfo> &files)
         }
 
         // File Name
-        dirEntryMustashe.set("fileName", files.at(fileIndex).fileName().toStdString());
+        QString filename = files.at(fileIndex).dir().dirName()+ "/" + files.at(fileIndex).fileName();
+        dirEntryMustashe.set("fileName", filename.toStdString());
         QUrl url{QUrl::fromLocalFile(files.at(fileIndex).filePath())};
         dirEntryMustashe.set("fileIndex", url.toString().toStdString());
+        dirEntryMustashe.set("search", files.at(fileIndex).baseName().toStdString());
 
         dirEntryListMustashe << dirEntryMustashe;
     }
