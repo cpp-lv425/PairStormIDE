d0c1402ecd0fd74f1870322464274b15e6a50f47
DANIV18 <igordaniv242@gmail.com>
2019-08-02 11:04:15 +0300

    Update simple test.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,48 @@
+#include <QDir>
+#include <QTime>
+#include <QDebug>
 #include <QApplication>
+#include "connectionmanager.h"
+#include <QStandardPaths>
+#include <QTextStream>
+#include <QWebEngineView>
+#include <QWebEngineSettings>
+#include <QWebEngineProfile>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <QCommandLineParser>
 
+#include "browser.h"
 #include "mainwindow.h"
+#include "documentationsearch.h"
+#include "documentationviewer.h"
+#include "htmlcontentgenerator.h"
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    w.showStartPage();
+
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::DnsPrefetchEnabled, true);
+    QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::LocalStorageEnabled, true);
+
+   // DocumentationViewer w;
+  //  w.show();
+    //DocumentationViewer view;
+  //  view.show();
+    //..QWebEngineView w;
+    //w.setUrl(QUrl("http://www.google.com"));
+    //w.show();
+    Browser b;
+           b.newTab("array");
+           b.newTab("vector");
+               b.show();
+//    b.newTab("daniv");
+//    b.newTab("Danissimo");
 
     return a.exec();
 }
