cf632b8d8ebe41b40688fd724884ab2ebc7652d6
Igor <32616149+DANIV18@users.noreply.github.com>
2019-08-05 16:32:34 +0300

    Update ps1 script.

--- windowsreference.ps1
+++ windowsreference.ps1
@@ -1,19 +1,20 @@
-$path = "$PSScriptRoot\res"
-If(!(test-path $path))
+$documents = [environment]::getfolderpath("mydocuments")
+$path = "$documents\PairStorm"
+If(!(Test-Path $path))
 {
       New-Item -ItemType Directory -Force -Path $path
 }
-if (!(Test-Path "$PSScriptRoot\res\reference"))
+if (!(Test-Path "$path\reference"))
 {
 	$url = "http://upload.cppreference.com/mwiki/images/b/b2/html_book_20190607.zip"
-	$output = "$PSScriptRoot\res\cppref.zip"
+	$output = "$path\cppref.zip"
 
 	$wc = New-Object System.Net.WebClient
 	$wc.DownloadFile($url, $output)
 #OR
 	(New-Object System.Net.WebClient).DownloadFile($url, $output)
-	Expand-Archive "$PSScriptRoot\res\cppref.zip"
-	Remove-Item "$PSScriptRoot\res\cppref.zip"
-	Move-Item -Path $PSScriptRoot\cppref\reference -Destination $path
-	Remove-Item -LiteralPath "$PSScriptRoot\cppref" -Force -Recurse
+	Expand-Archive -Path "$path\cppref.zip" -DestinationPath "$path"
+	Remove-Item "$path\cppref.zip"
+	Move-Item -Path "$path\cppref\reference" -Destination $path
+	Remove-Item -LiteralPath "$path\cppref" -Force -Recurse
 }
