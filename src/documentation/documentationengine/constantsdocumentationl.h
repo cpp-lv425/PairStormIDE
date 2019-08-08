3b96ea17a6d53f3592e8a088606bf7c1c357ea30
DANIV18 <igordaniv242@gmail.com>
2019-08-08 13:39:11 +0300

    Add styles to html page

--- src/documentation/documentationengine/constantsdocumentationl.h
+++ src/documentation/documentationengine/constantsdocumentationl.h
@@ -11,6 +11,58 @@ namespace ReferenceUrl
 const std::string mustasheTemplate{R"(<!DOCTYPE html>
                                     <html>
                                         <head>
+                                   <style>
+                                   * {
+                                       text-align: center;
+                                       padding-right: 25%;
+                                       padding-left: 25%;
+                                       font-family: "Courier New", monospace;
+                                   }
+
+                                   table {
+                                       font-size: 18px;
+                                       background: white;
+                                       max-width: 100%;
+                                       border-collapse: collapse;
+                                       text-align: left;
+                                   }
+
+                                   tr:first-child {
+                                       font-weight: normal;
+                                       border-bottom: 2px solid gray;
+                                       padding: 10px 8px;
+                                       text-align: left;
+                                       background-color: white;
+                                   }
+
+                                   tr:last-child {
+                                       font-weight: normal;
+                                       border-bottom: 2px solid gray;
+                                   }
+
+                                   tr:first-child td {
+                                       font-size: 25px;
+                                      color: rgba(0, 0, 0, .8);
+                                      padding: 9px 8px;
+                                      text-align: center;
+                                   }
+
+                                   td {
+                                      color: rgba(0, 0, 0, .5);
+                                      padding: 9px 8px;
+                                      text-align: left;
+                                   }
+
+                                   td a {
+                                       margin-left: -84px;
+                                       color: rgba(0, 0, 0, .6);
+                                       text-decoration: none;
+                                   }
+
+                                   td a:hover {
+                                       color: rgba(0, 0, 0, .9);
+                                   }
+                                   </style>
                                             <title>{{search}}</title>
                                             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                                             </head>
