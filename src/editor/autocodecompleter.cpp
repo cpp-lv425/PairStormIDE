3ce84e7b5735c198b9dbd8d96ae4a68f8b666f3e
PeterLysyk <lyssykpetro@gmail.com>
2019-08-21 20:06:59 +0300

    returned to the static_cast version

--- src/editor/autocodecompleter.cpp
+++ src/editor/autocodecompleter.cpp
@@ -9,14 +9,7 @@ AutoCodeCompleter::AutoCodeCompleter(const QStringList &completions, QObject *pa
     connect(this, SIGNAL(activated(QString)), this, SLOT(replaceCurrentWord(QString)));
 }
 
-/*
-1) rewrite signal
-2) rewrite function returns
-3) rewrite if/else parts (create special fucntions for that)
-4) rewrite it as key
-*/
-
-/*bool AutoCodeCompleter::eventFilter(QObject *object, QEvent *event)
+bool AutoCodeCompleter::eventFilter(QObject *object, QEvent *event)
 {
     if (event->type() == QEvent::KeyPress)
     {
@@ -56,47 +49,6 @@ AutoCodeCompleter::AutoCodeCompleter(const QStringList &completions, QObject *pa
             }
             return true;
         }
-    }
-    return QCompleter::eventFilter(object, event);
-}*/
-
-bool AutoCodeCompleter::eventFilter(QObject *object, QEvent *event)
-{
-    QKeyEvent* keyEvent = dynamic_cast<QKeyEvent*>(event);
-    if (!keyEvent)
-    {
-        return QCompleter::eventFilter(object, event);
-    }
-
-    if (isNotEnterKey(keyEvent))
-    {
-        QPlainTextEdit *textEdit = qobject_cast<QPlainTextEdit*>(widget());
-        QTextCursor textCursor = textEdit->textCursor();
-
-        textCursor.movePosition(QTextCursor::StartOfWord, QTextCursor::KeepAnchor);
-
-        if (textCursor.selectedText().length() >= getMinCompletionPrefixLength()
-                && !isUpDownKey(keyEvent))
-        {
-            setCompletionPrefix(textCursor.selectedText());
-            QRect rect = QRect(textEdit->cursorRect().bottomLeft(), QSize(100, 5));
-            complete(rect);
-            if (keyEvent->key() == Qt::Key_Space)
-            {
-                popup()->hide();
-            }
-        }
-        return QCompleter::eventFilter(object, event);;
-    }
-
-    if (popup()->isVisible())
-    {
-        popup()->hide();
-        if (popup()->currentIndex().isValid())
-        {
-            emit activated(popup()->currentIndex().data(completionRole()).toString());
-        }
-        return true;
     }
     return QCompleter::eventFilter(object, event);
 }
